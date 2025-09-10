423120f84f537e9ff31570b10d6c5f5f83f19130
olisaxh <olisaxhh01@gmail.com>
2025-09-10 17:13:16 +0200

    ff

---
+++ LESSON28/js/main.js
@@ -0,0 +1,29 @@
+var button1 = document.getElementById('btn1');
+
+button1.oneclick =function(){
+    alert('hello')
+}
+
+// onemouseover
+// onemouseleave
+// onemousewheel
+
+var button2 = document.getElementById('btn1');
+
+button2.onemouseover =function(){
+    alert('hello')
+}
+
+
+var button3 = document.getElementById('btn1');
+
+button3.onemouseleave =function(){
+    alert('hello')
+}
+
+
+var button4 = document.getElementById('btn1');
+
+button4.onemousewheel =function(){
+    alert('hello')
+}
