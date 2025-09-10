e75a5a8ed605fc4c997fdf7a5d60452cb2c06c64
olisaxh <olisaxhh01@gmail.com>
2025-09-10 17:01:01 +0200

    TRE

---
+++ module27/js/main.js
@@ -0,0 +1,9 @@
+
+function Computer (name , CPU , RAM ,GPU) {
+    this.name=name;
+    this.CPU=CPU;
+    this.RAM=RAM;
+    this.GPU=GPU;
+}
+
+var Computer = new Computer("MacBook" , "M8- core" ,"8GB" , "GPU")
