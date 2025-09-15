409fdcb64771548f58d43f9d8d8211794f8864e9
olisaxh <olisaxhh01@gmail.com>
2025-09-15 17:44:53 +0200

    done

---
+++ lesson29/jss/main.js
@@ -0,0 +1,17 @@
+var programminglanguage = ['java','php', 'python']
+
+console.log(programminglanguage[0])
+console.log(programminglanguage[1])
+console.log(programminglanguage[2])
+
+ programminglanguage.push(javascript)
+ console.log(programminglanguage[3])
+
+  programminglanguage.pop()
+
+   programminglanguage.unshift(javascript)
+ console.log(programminglanguage)
+
+   programminglanguage.shift()
+
+   programminglanguage.slice(0, 1, 'rubys')
