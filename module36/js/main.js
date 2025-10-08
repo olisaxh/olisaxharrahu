e84f3d43c8236273e0408b9a086a9b59e320920f
olisaxh <olisaxhh01@gmail.com>
2025-10-08 17:46:51 +0200

    done

---
+++ module36/js/main.js
@@ -0,0 +1,16 @@
+$(window).ready(function() {
+  alert ("is this working");
+});
+
+$('#header')
+$('li')
+$('ul li')
+$(',student')
+
+
+$('a.test:first')
+$('tr:odd')
+$('#myForm :input')
+$('div:visible')
+$('div:gt(2)')
+$('div:animated')
