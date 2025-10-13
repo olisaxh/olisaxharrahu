f7f0eeceb28b6dff2ffd68ecec8b90b76b02bddd
olisaxh <olisaxhh01@gmail.com>
2025-10-13 17:39:59 +0200

    thg

---
+++ module36/js1/main.js
@@ -0,0 +1,49 @@
+$('li').click(function(){
+    alert('clicked');
+});
+
+$('#btn').click(function(){
+    console.log($('#h1').text());
+    $('#h1').text('testing test');
+     $('#h1').append('extra test');
+})
+
+$('#btn2').on(click, function(){
+    console.log('Second button is clicked');
+});
+
+$('#myDiv').click(function(){
+    $(this).addClass('clicked');
+}) 
+.find('span')
+.attr('title', 'Hover over me');
+
+$('#btn3').click(function(){
+    $(".hidden").show('slow');
+});
+
+$('#btn4').click(function(){
+    $(".hidden").hide('slow');
+});
+
+$('#square').click(function(){
+   
+    $('#square').animate({
+        'width':'200px',
+        'height':'200px'
+    });
+});
+
+$('.testing').click(function(){
+
+    $('.testing').animate({
+        'left': '+ 50',
+        'opacity': 0.25,
+        'fontsize':'30px'
+    },
+    300,
+    function(){
+        console.log("animation is complete")
+    }
+);
+});
